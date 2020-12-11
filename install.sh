25eabbfc95ab7d9ddb5932c4c7c148c107a1c739
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2020-12-11 16:17:39 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#@Author prince kumar
+#Date 10 dec 2020
+#VersionV1.0
+apt update && apt upgrade
+apt-git install python
+apt-git install python2
+pkg install python3
+pip install -r requirements.txt
+pip install tqdm
+pip install socket
+clear
+python3 Tsacn.py
